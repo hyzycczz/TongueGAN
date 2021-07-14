03dc883c0792a9f21063e142cd51a9cb6bd276db
minired2154 <st96179@gmail.com>
2021-07-15 00:51:52 +0800

    update test.py

--- 測試/test.py
+++ 測試/test.py
@@ -4,11 +4,18 @@ import matplotlib.pyplot as plt
 
 from scipy import signal
 
-x = np.arange(0,1,0.001)
-y = np.cos(x)
-z = np.sin(x)
+x = np.arange(0,1,0.1)
+y = np.arange(0,1,0.1)
+z = [[] for i in range(len(x))]
 
-plt.pcolormesh(x, y,z, vmin=0, vmax=5, shading='flat')
+
+for i in range(len(x)):
+    for j in range(len(y)):
+        z[i].append(x[i] + y[j])
+
+
+
+plt.pcolormesh(x, y,z, vmin=0, vmax=2, shading='flat')
 plt.title('STFT Magnitude')
 plt.ylabel('Frequency [Hz]')
 plt.xlabel('Time [sec]')
