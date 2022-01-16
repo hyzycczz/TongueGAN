c5ae879aac3c1f49bc13ca86f6f541a2334424c2
minired2154 <st96179@gmail.com>
2022-01-16 14:21:17 +0800

    update the WGAN code

--- 測試/CastleGAN/GAN.py
+++ 測試/CastleGAN/GAN.py
@@ -1,3 +1,4 @@
+from ast import Load
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
@@ -5,7 +6,7 @@ from tensorflow.keras import layers
 import yaml
 from yaml import Loader
 
-from loader import load_dataset, load_discriminator, load_generator, GANMonitor, GAN
+from loader import load_dataset, load_discriminator, load_generator, WGAN, GANMonitor, generator_loss, discriminator_loss
 
 
 if __name__ == "__main__":
@@ -36,4 +37,29 @@ if __name__ == "__main__":
 
     # --------------DCGAN END-------------- '''
 
-    
+
+    with open('config.yml', 'r') as f:
+        config = yaml.load(f, Loader=Loader)
+
+    dataset = load_dataset(config['dataset'])
+    print(dataset.element_spec)
+
+    latent_dim = 128
+    Generator = load_generator(latent_dim)
+    Discriminator = load_discriminator()
+
+    epochs = 2000
+
+    gan = WGAN(discriminator=Discriminator, 
+        generator=Generator,
+        latent_dim=latent_dim,
+        discriminator_extra_steps=5)
+    gan.compile(
+        d_optimizer=keras.optimizers.Adam(learning_rate=0.0001),
+        g_optimizer=keras.optimizers.Adam(learning_rate=0.0001),
+        g_loss_fn=generator_loss,
+        d_loss_fn=discriminator_loss,
+    )
+    gan.fit(
+        dataset, epochs=epochs, callbacks=[GANMonitor(num_img=3, latent_dim=latent_dim)]
+    )
--- 測試/CastleGAN/loader.py
+++ 測試/CastleGAN/loader.py
@@ -17,7 +17,7 @@ def load_dataset(config):
         shuffle = config['suffle']
     )
     return train_ds
-
+''' --------------DCGAN START--------------
 def load_discriminator():
     discriminator = keras.Sequential(
         [
@@ -137,3 +137,181 @@ class GAN(keras.Model):
             "d_loss": self.d_loss_metric.result(),
             "g_loss": self.g_loss_metric.result(),
         }
+
+# --------------DCGAN END-------------- '''
+
+'''-------------WGAN START-------------'''
+def load_discriminator():
+    discriminator = keras.Sequential(
+        [
+            keras.Input(shape=(64, 64, 3)),
+            layers.Conv2D(64, kernel_size=4, strides=2, padding="same"),
+            layers.LeakyReLU(alpha=0.2),
+            layers.Conv2D(128, kernel_size=4, strides=2, padding="same"),
+            layers.LeakyReLU(alpha=0.2),
+            layers.Conv2D(128, kernel_size=4, strides=2, padding="same"),
+            layers.LeakyReLU(alpha=0.2),
+            layers.Flatten(),
+            layers.Dropout(0.2),
+            layers.Dense(1),
+        ],
+        name="discriminator",
+    )
+    discriminator.summary()
+    return discriminator
+
+def load_generator(latent_dim):
+    generator = keras.Sequential(
+        [
+            keras.Input(shape=(latent_dim,)),
+            layers.Dense(8 * 8 * 128),
+            layers.Reshape((8, 8, 128)),
+            layers.Conv2DTranspose(128, kernel_size=4, strides=2, padding="same"),
+            layers.LeakyReLU(alpha=0.2),
+            layers.Conv2DTranspose(256, kernel_size=4, strides=2, padding="same"),
+            layers.LeakyReLU(alpha=0.2),
+            layers.Conv2DTranspose(512, kernel_size=4, strides=2, padding="same"),
+            layers.LeakyReLU(alpha=0.2),
+            layers.Conv2D(3, kernel_size=5, padding="same", activation="sigmoid"),
+        ],
+        name="generator",
+    )
+    generator.summary()
+    return generator
+
+class WGAN(keras.Model):
+    def __init__(
+        self,
+        discriminator,
+        generator,
+        latent_dim,
+        discriminator_extra_steps=3,
+        gp_weight=10.0,
+    ):
+        super(WGAN, self).__init__()
+        self.discriminator = discriminator
+        self.generator = generator
+        self.latent_dim = latent_dim
+        self.d_steps = discriminator_extra_steps
+        self.gp_weight = gp_weight
+
+    def compile(self, d_optimizer, g_optimizer, d_loss_fn, g_loss_fn):
+        super(WGAN, self).compile()
+        self.d_optimizer = d_optimizer
+        self.g_optimizer = g_optimizer
+        self.d_loss_fn = d_loss_fn
+        self.g_loss_fn = g_loss_fn
+
+    def gradient_penalty(self, batch_size, real_images, fake_images):
+        """ Calculates the gradient penalty.
+
+        This loss is calculated on an interpolated image
+        and added to the discriminator loss.
+        """
+        # Get the interpolated image
+        alpha = tf.random.normal([batch_size, 1, 1, 1], 0.0, 1.0)
+        diff = fake_images - real_images
+        interpolated = real_images + alpha * diff
+
+        with tf.GradientTape() as gp_tape:
+            gp_tape.watch(interpolated)
+            # 1. Get the discriminator output for this interpolated image.
+            pred = self.discriminator(interpolated, training=True)
+
+        # 2. Calculate the gradients w.r.t to this interpolated image.
+        grads = gp_tape.gradient(pred, [interpolated])[0]
+        # 3. Calculate the norm of the gradients.
+        norm = tf.sqrt(tf.reduce_sum(tf.square(grads), axis=[1, 2, 3]))
+        gp = tf.reduce_mean((norm - 1.0) ** 2)
+        return gp
+
+    def train_step(self, real_images):
+        if isinstance(real_images, tuple):
+            real_images = real_images[0]
+
+        # Get the batch size
+        batch_size = tf.shape(real_images)[0]
+
+        # For each batch, we are going to perform the
+        # following steps as laid out in the original paper:
+        # 1. Train the generator and get the generator loss
+        # 2. Train the discriminator and get the discriminator loss
+        # 3. Calculate the gradient penalty
+        # 4. Multiply this gradient penalty with a constant weight factor
+        # 5. Add the gradient penalty to the discriminator loss
+        # 6. Return the generator and discriminator losses as a loss dictionary
+
+        # Train the discriminator first. The original paper recommends training
+        # the discriminator for `x` more steps (typically 5) as compared to
+        # one step of the generator. Here we will train it for 3 extra steps
+        # as compared to 5 to reduce the training time.
+        for i in range(self.d_steps):
+            # Get the latent vector
+            random_latent_vectors = tf.random.normal(
+                shape=(batch_size, self.latent_dim)
+            )
+            with tf.GradientTape() as tape:
+                # Generate fake images from the latent vector
+                fake_images = self.generator(random_latent_vectors, training=True)
+                # Get the logits for the fake images
+                fake_logits = self.discriminator(fake_images, training=True)
+                # Get the logits for the real images
+                real_logits = self.discriminator(real_images, training=True)
+
+                # Calculate the discriminator loss using the fake and real image logits
+                d_cost = self.d_loss_fn(real_img=real_logits, fake_img=fake_logits)
+                # Calculate the gradient penalty
+                gp = self.gradient_penalty(batch_size, real_images, fake_images)
+                # Add the gradient penalty to the original discriminator loss
+                d_loss = d_cost + gp * self.gp_weight
+
+            # Get the gradients w.r.t the discriminator loss
+            d_gradient = tape.gradient(d_loss, self.discriminator.trainable_variables)
+            # Update the weights of the discriminator using the discriminator optimizer
+            self.d_optimizer.apply_gradients(
+                zip(d_gradient, self.discriminator.trainable_variables)
+            )
+
+        # Train the generator
+        # Get the latent vector
+        random_latent_vectors = tf.random.normal(shape=(batch_size, self.latent_dim))
+        with tf.GradientTape() as tape:
+            # Generate fake images using the generator
+            generated_images = self.generator(random_latent_vectors, training=True)
+            # Get the discriminator logits for fake images
+            gen_img_logits = self.discriminator(generated_images, training=True)
+            # Calculate the generator loss
+            g_loss = self.g_loss_fn(gen_img_logits)
+
+        # Get the gradients w.r.t the generator loss
+        gen_gradient = tape.gradient(g_loss, self.generator.trainable_variables)
+        # Update the weights of the generator using the generator optimizer
+        self.g_optimizer.apply_gradients(
+            zip(gen_gradient, self.generator.trainable_variables)
+        )
+        return {"d_loss": d_loss, "g_loss": g_loss}
+
+def discriminator_loss(real_img, fake_img):
+    real_loss = tf.reduce_mean(real_img)
+    fake_loss = tf.reduce_mean(fake_img)
+    return fake_loss - real_loss
+
+
+# Define the loss functions for the generator.
+def generator_loss(fake_img):
+    return -tf.reduce_mean(fake_img)
+
+class GANMonitor(keras.callbacks.Callback):
+    def __init__(self, num_img=6, latent_dim=128):
+        self.num_img = num_img
+        self.latent_dim = latent_dim
+
+    def on_epoch_end(self, epoch, logs=None):
+        random_latent_vectors = tf.random.normal(shape=(self.num_img, self.latent_dim))
+        generated_images = self.model.generator(random_latent_vectors)
+        generated_images = (generated_images * 127.5) + 127.5
+
+        for i in range(self.num_img):
+            img = generated_images[i].numpy()
+            img = keras.preprocessing.image.array_to_img(img)
+            img.save("generated_img_{i}_{epoch}.png".format(i=i, epoch=epoch))
